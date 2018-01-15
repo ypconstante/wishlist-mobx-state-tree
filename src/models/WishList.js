f38e6e1e0b2a96a853bc993c055fef0cbaa70046
Yuri Pereira Constante <ypconstante@gmail.com>
2018-01-14 22:26:45 -0200

    01 - Describe Your Application Domain Using mobx-state-tree(MST) Models

---
+++ src/models/WishList.js
@@ -0,0 +1,12 @@
+import { types } from "mobx-state-tree";
+
+export const WishListItem = types
+    .model({
+        name: types.string,
+        price: types.number,
+        image: "",
+    });
+
+export const WishList = types.model({
+    items: types.optional(types.array(WishListItem), []),
+});
