4590116d524ba3758913f1ed2f0a712cdc62bf15
Yuri Pereira Constante <ypconstante@gmail.com>
2018-01-25 16:27:38 -0200

    08 - Create an Entry Form to Add Models to the State Tree

---
+++ src/components/WishListItemEntry.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+import WishListItemEdit from "./WishListItemEdit";
+import { WishListItem } from "./../models/WishList";
+
+class WishListItemEntry extends Component {
+    constructor() {
+        super();
+        this.state = {
+            entry: WishListItem.create({
+                name: "",
+                price: 0,
+            }),
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <WishListItemEdit item={this.state.entry} />
+                <button onClick={this.onAdd}>Add</button>
+            </div>
+        )
+    }
+
+    onAdd = () => {
+        this.props.wishList.add(this.state.entry);
+        this.setState({
+            entry: WishListItem.create({
+                name: "",
+                price: 0,
+            }),
+        });
+    }
+}
+
+export default WishListItemEntry;
--- src/components/WishListView.js
+++ src/components/WishListView.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { observer } from "mobx-react";
 
-import WishListItemView from "./WishListItemView"
+import WishListItemView from "./WishListItemView";
+import WishListItemEntry from "./WishListItemEntry";
 import "./WishListView.css";
 
 const WishListView = ({ wishList }) => (
@@ -11,6 +12,8 @@ const WishListView = ({ wishList }) => (
                 (<WishListItemView key={index} item={item} />)
             )}
             Total: ${wishList.totalPrice}
+
+            <WishListItemEntry wishList={wishList} />
         </ul>
     </div>
 );
