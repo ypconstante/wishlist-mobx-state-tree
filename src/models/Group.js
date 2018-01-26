f5ced84ec24f12bb41c9ebb6b1d4dea4fc2c4f8e
Yuri Pereira Constante <ypconstante@gmail.com>
2018-01-26 17:11:39 -0200

    15 - Use Volatile State and Lifecycle Methods to Manage Private State

--- src/models/Group.js
+++ src/models/Group.js
@@ -22,42 +22,76 @@ export const Group = types
     .model({
         users: types.map(User),
     })
-    .actions(self => ({
-        afterCreate() {
-            self.load();
-        },
-        load: flow(function* () {
-            const response = yield fetch(`http://localhost:3001/users`);
-            const users = yield response.json();
-            applySnapshot(self.users, users);
-        }),
-        drawLots() {
-            const allUsers = self.users.values();
+    .actions(self => {
+        let abortController;
+        let idLastRequest = 0;
 
-            if (allUsers.length <= 1)
-                return;
+        return {
+            afterCreate() {
+                self.load();
+            },
+            beforeDestroy() {
+                console.log(`aborting request ${idLastRequest} thru beforeDestroy`)
+                abortRequest();
+            },
+            load: flow(function* () {
+                let idCurrentRequest = ++idLastRequest;
+                abortController = window.AbortController && new window.AbortController();
+                try {
+                    console.log(`start request ${idCurrentRequest}`, abortController);
+                    const response = yield fetch(
+                        `http://localhost:3001/users`,
+                        { signal: abortController && abortController.signal }
+                    );
+                    abortController = null;
+                    const users = yield response.json();
+                    applySnapshot(self.users, users);
+                    console.log(`finished request ${idCurrentRequest}`);
+                } catch (e) {
+                    console.log(`aborted request ${idCurrentRequest}`, e);
+                }
+            }),
+            reload() {
+                abortRequest();
+                self.load();
+            },
+            drawLots() {
+                const allUsers = self.users.values();
 
-            const remaining = [...allUsers];
+                if (allUsers.length <= 1)
+                    return;
 
-            allUsers.forEach((user) => {
-                user.recipient = null;
-                if (remaining.length === 1 && remaining[0] === user) {
-                    // last user on the list and only remaining
-                    const indexSwapWith = Math.floor(Math.random() * (allUsers.length - 1));
-                    const swapWith = allUsers[indexSwapWith];
-                    user.recipient = swapWith.recipient;
-                    swapWith.recipient = user;
-                } else {
-                    while (user.recipient == null) {
-                        const indexRecipient = Math.floor(Math.random() * remaining.length);
-                        const recipient = remaining[indexRecipient];
+                const remaining = [...allUsers];
 
-                        if (recipient !== user) {
-                            user.recipient = recipient;
-                            remaining.splice(indexRecipient, 1);
+                allUsers.forEach((user) => {
+                    user.recipient = null;
+                    if (remaining.length === 1 && remaining[0] === user) {
+                        // last user on the list and only remaining
+                        const indexSwapWith = Math.floor(Math.random() * (allUsers.length - 1));
+                        const swapWith = allUsers[indexSwapWith];
+                        user.recipient = swapWith.recipient;
+                        swapWith.recipient = user;
+                    } else {
+                        while (user.recipient == null) {
+                            const indexRecipient = Math.floor(Math.random() * remaining.length);
+                            const recipient = remaining[indexRecipient];
+
+                            if (recipient !== user) {
+                                user.recipient = recipient;
+                                remaining.splice(indexRecipient, 1);
+                            }
                         }
                     }
-                }
-            })
+                })
+            }
         }
-    }));
+
+        function abortRequest() {
+            if (abortController) {
+                abortController.abort();
+                console.log(`aborting request ${idLastRequest}`);
+            } else {
+                console.log(`abort skipped for request ${idLastRequest}`);
+            }
+        }
+    });
